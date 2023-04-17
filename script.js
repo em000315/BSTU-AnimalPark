8c58a0090ac8dca16f6eed337667d85491db52ae
em000315 <em000315@g.bstu.by>
2023-04-17 17:41:37 +0300

    feat: add script
    Добавлен скрипт переключающий кнопки с фотографиями

---
+++ script.js
@@ -0,0 +1,50 @@
+const animals = {
+    large: [
+      'assets/jpg/1.png',
+      'assets/jpg/4.jpg',
+      'assets/jpg/cow.jpg',
+      'assets/jpg/lama.jpg',
+      'assets/jpg/tiger.jpg',
+      'assets/jpg/giraf.jpg',
+    ],
+    fluffy: [
+      'assets/jpg/2.jpg',
+      'assets/jpg/3.jpg',
+      'assets/jpg/5.jpg',
+      'assets/jpg/raccon.jpg',
+      'assets/jpg/bear.jpg',
+      'assets/jpg/belka.jpg',
+    ],
+    birds: [
+      'assets/jpg/6.png',
+      'assets/jpg/yellow_parrot.jpg',
+      'assets/jpg/crane.jpg',
+      'assets/jpg/flamingo.jpg',
+      'assets/jpg/owl.jpg',
+      'assets/jpg/parrot.jpg',
+    ],
+  }
+  
+  function changeImage (event) {
+    const animal = event.target.dataset.animal;
+    const animalImages = document.querySelector('.imagesOfAnimals').children;
+    Array.from(animalImages).forEach((image, index) => image.src = animals[animal][index]);
+    
+    const staticButtons = document.querySelector('.orangeBlock').children;
+    Array.from(staticButtons).forEach(staticButton => {
+      if (staticButton.dataset.animal == animal) {
+        staticButton.classList.add('button_color');
+      } else {
+        staticButton.classList.remove('button_color');
+      };
+    });
+    
+  };
+  
+  function buttonClick (event) {
+    if (event.target.classList.contains('staticButton')) {
+      changeImage (event);
+    };
+  };
+  
+  document.querySelector('.orangeBlock').addEventListener('click', buttonClick);
